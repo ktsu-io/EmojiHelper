edb64055589babc75e68b8eaf6b34e71a52f9d35
matt-edmondson <matthew.edmondson@gmail.com>
2024-05-08 11:39:51 +1000

    Fix style errors

--- EmojiHelper/Codepoint.cs
+++ EmojiHelper/Codepoint.cs
@@ -1,37 +1,25 @@
-namespace EmojiHelper
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace EmojiHelper;
+
+public class Codepoint
 {
-	public class Codepoint
+	private readonly string code;
+
+	public Codepoint(string codepoint)
 	{
-		private readonly string _code;
-
-		public Codepoint(string codepoint)
-		{
-			// remove any charachters that are not hex digits by replacing them with spaces
-			string hexDigits = new(codepoint.Select(c => Uri.IsHexDigit(c) ? c : ' ').ToArray());
-			// remove repeated spaces
-			string[] segments = hexDigits.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-			// join the segments with a space
-			_code = string.Join(" ", segments).ToUpperInvariant();
-		}
-
-		public override bool Equals(object? obj)
-		{
-			if (obj is Codepoint other)
-			{
-				return _code == other._code;
-			}
-
-			return false;
-		}
-
-		public override int GetHashCode()
-		{
-			return _code.GetHashCode();
-		}
-
-		public override string ToString()
-		{
-			return _code;
-		}
+		// remove any charachters that are not hex digits by replacing them with spaces
+		string hexDigits = new(codepoint.Select(c => Uri.IsHexDigit(c) ? c : ' ').ToArray());
+		// remove repeated spaces
+		string[] segments = hexDigits.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+		// join the segments with a space
+		code = string.Join(" ", segments).ToUpperInvariant();
 	}
+
+	public override bool Equals(object? obj) =>
+		obj is Codepoint other && code == other.code;
+
+	public override int GetHashCode() => code.GetHashCode(StringComparison.Ordinal);
+
+	public override string ToString() => code;
 }
